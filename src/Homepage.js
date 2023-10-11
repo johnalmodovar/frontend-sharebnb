00277302e09fa551b27dcbfd26a932d9f64a626a
Megan <mvyenielo58@gmail.com>
2023-10-11 14:40:02 -0700

    added login form component and homepage component

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import userContext from "./userContext";
 
+/**
+ * Homepage: Renders welcome message to logged in users or links to signup/login
+ *
+ * RoutesList => Homepage
+ *
+ */
 function Homepage() {
+  const { currentUser } = useContext(userContext);
+
+  //TODO: SearchForm component on homepage
+  //TODO: add slogan
+
+  return (
+    <div className="Homepage">
+      <h1>ShareBnB</h1>
+      {currentUser ?
+        <h2>Hello {currentUser.username}!</h2> :
+        <>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
+        </>}
+    </div>
+  );
 
 }
 
--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,6 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function LoginForm() {
+/**
+ * LoginForm: Renders for for user to log in
+ *
+ * Displays errors if user info is incorrect
+ *
+ * Props:
+ * - login: function that sends user login info to update state in main app
+ *
+ * State:
+ * - loginData: object that holds/updates user username and login info
+ * {username: "", password: ""}
+ *
+ * - formErrors: If errors returned from login attempt, holds array of error
+ *   messages
+ * ["error", ...]
+ *
+ * RoutesList => LoginForm => Alert
+ *
+ */
+
+function LoginForm({ login }) {
+  const navigate = useNavigate();
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: ""
+  });
+  const [formErrors, setFormErrors] = useState([]);
+
+  /** handleChange: updates login information in state as user types in input */
+  function handleChange(evt) {
+    const { name, value } = evt.target;
+    setLoginData(l => ({ ...l, [name]: value }));
+  }
+  /** Handles login form submit
+   * Calls login function and sends data to App
+   * OR
+   * if errors, displays errors
+   */
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+    try {
+      await login(loginData);
+      navigate("/");
+    } catch (err) {
+      setFormErrors(err);
+    }
+  }
+
+  return (
+    <div className="LoginForm">
+      <h2>Log In</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="LoginForm-username">
+          <label htmlFor="username" className="form-label">Username</label>
+          <input
+            name="username"
+            value={loginData.username}
+            onChange={handleChange}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="LoginForm-password">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input
+            name="password"
+            value={loginData.password}
+            onChange={handleChange}
+            className="form-control"
+            required
+            type="password"
+          />
+        </div>
+        <button>Submit</button>
+      </form>
+      {/* TODO: Add conditional for errors, add Error/Alert component */}
+    </div>
+  );
 
 }
 
